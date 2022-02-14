425473f98eeed5e230f9f81f461729593894c7b0
Galochkin D.D <galochkin.dd@gmail.com>
2022-02-14 03:55:08 +0300

    Experiments: added two views to storyboard (one from xib file and with autoresizing mask)

---
+++ Navigation/CustomView.swift
@@ -0,0 +1,15 @@
+//
+//  CustomView.swift
+//  Navigation
+//
+//  Created by Дмитрий Галочкин on 14.02.2022.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+    @IBOutlet weak var CustomSwitch: UISwitch!
+    
+}
+
--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var CustomView1: UIView!
+    @IBOutlet weak var CustomView2: CustomView!
+    
+    @IBOutlet weak var CustomLabel: UILabel!
+    @IBOutlet weak var CustomButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.CustomLabel.text = "MyCustomLabel"
+        self.CustomButton.setTitle("MyCustomButton", for: .normal)
+        
+        if let CustomView = Bundle.main.loadNibNamed("CustomView",
+                                                     owner: nil,
+                                                     options: nil)?.first as? UIView {
+            print("CustomView loaded")
+            print("CustomView loaded")
+            print("CustomView loaded")
+
+            CustomView2.addSubview(CustomView)
+
+        }
+        
+        
+        // UIStoryboard(name: "Main", bundle: "MyStoryBoard")
+        
     }
-
-
+    
+    @IBAction func didTapButton(_ sender: UIButton) {
+        print(#function)
+    }
+    
+    
+    
 }
 
