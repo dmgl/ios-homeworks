b8702d596d58d66ffbc37b96bf8212218c00b37a
Galochkin D.D <galochkin.dd@gmail.com>
2022-03-17 00:25:04 +0300

    Trying to correct mistakes

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -8,39 +8,21 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        //self.view.backgroundColor = .purple
-
-        //self.view.autoresizesSubviews = true
-
+        
         if let profileViewFromXib = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first
             as? UIView {
 
             self.view.addSubview(profileViewFromXib)
-            
-            //profileViewFromXib.backgroundColor = .yellow
-            //profileViewFromXib.profileName = "How to change this???"
-            
+
             profileViewFromXib.frame = self.view.bounds
-
-            //self.view.frame = self.view.frame.inset(by: UIEdgeInsets(top: -5, left: -50,
-            //                                                         bottom: -1, right: -1)) // no matter because flexibleWidth, flexibleHeight
-
             profileViewFromXib.autoresizingMask = [.flexibleWidth,
                                                     .flexibleHeight]
-//            profileViewFromXib.autoresizingMask = [.flexibleWidth,
-//                                                    .flexibleHeight,
-//                                                    .flexibleLeftMargin,
-//                                                    .flexibleRightMargin,
-//                                                    .flexibleTopMargin,
-//                                                    .flexibleBottomMargin]
-             
-            
-            }
-        
+    
+        }
         
     }
 }
