20804ba92943e211cac826724d9890456ce9d67d
Galochkin D.D <galochkin.dd@gmail.com>
2022-03-20 04:58:23 +0300

    Result of homework task

--- Navigation/AlertViewController.swift
+++ Navigation/AlertViewController.swift
@@ -9,30 +9,30 @@ import UIKit
 
 class AlertViewController: UIAlertController {
 
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
+
         self.title = "Delete post immediately?"
         self.message = "Choose your decision"
-        
+
         print("Delete post immediately?")
-    
+
         let deleteConfirmation = UIAlertAction(title: "Yes, delete", style: .destructive) {
             action in
             print("Yes, was deleted")
         }
-        
+
         let deleteAbort = UIAlertAction(title: "No", style: .cancel) {
             (_) in
             print("No, abort delete")
-            
+
         }
-        
+
         self.addAction(deleteConfirmation)
         self.addAction(deleteAbort)
     }
-    
-    
+
+
 }
