a8c3a4c9af6d3906d96e57ab1b5dfd95b4264454
Galochkin D.D <galochkin.dd@gmail.com>
2022-03-22 04:56:36 +0300

    Result of homework task

---
+++ Navigation/AlertController.swift
@@ -0,0 +1,38 @@
+//
+//  AlertViewController.swift
+//  Navigation
+//
+//  Created by Дмитрий Галочкин on 20.03.2022.
+//
+
+import UIKit
+
+class AlertController: UIAlertController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        self.title = "Delete post immediately?"
+        self.message = "Choose your decision"
+
+        print("Delete post immediately?")
+
+        let deleteConfirmation = UIAlertAction(title: "Yes, delete", style: .destructive) {
+            action in
+            print("Yes, was deleted")
+        }
+
+        let deleteAbort = UIAlertAction(title: "No", style: .cancel) {
+            (_) in
+            print("No, abort delete")
+
+        }
+
+        self.addAction(deleteConfirmation)
+        self.addAction(deleteAbort)
+    }
+
+
+}
